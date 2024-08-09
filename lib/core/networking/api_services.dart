32b7aa41aa56351c014e75130b127f3a7c949ff0
mohamed <alahly1907win@gmail.com>
2024-08-09 17:17:46 +0300

    ...

--- lib/core/networking/api_services.dart
+++ lib/core/networking/api_services.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_application_1/core/networking/api_constants.dart';
 import 'package:flutter_application_1/featuers/loginscreen/data/models/login_request_body.dart';
-import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'api_services.g.dart';
@@ -9,10 +8,10 @@ part 'api_services.g.dart';
 @RestApi(baseUrl: ConstansApi.apibaseUrl)
 abstract class ApiServices {
   factory ApiServices(Dio dio, {String baseUrl}) = _ApiServices;
-/// Post Method => request data .
+
+  /// Post Method => request data .
   @POST(ConstansApi.login)
   Future<String> login(
     @Body() LoginRequestBody loginRequestBody,
   );
-  
 }
